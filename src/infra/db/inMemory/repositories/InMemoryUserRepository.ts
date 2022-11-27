89884faa33e4166f0b80891a9913a590f5fc6cde
Jonathan Galdino <johnzballad@gmail.com>
2022-11-27 01:05:14 -0300

    creating infra/db folder and relatives

---
+++ src/infra/db/inMemory/repositories/InMemoryUserRepository.ts
@@ -0,0 +1,24 @@
+import { CreateUserRepository } from "@src/application/interfaces/repositories/user/CreateUserRepository";
+import { UserEntity, UserProps } from "@src/domain/entities/UserEntity";
+
+const inMemoryDatabase = new Map<string, UserProps>()
+
+// default user to test getUser() method
+const defaultUser = new UserEntity({ 
+  id: '321',
+  email: 'jaja@gmail.com'
+});
+inMemoryDatabase.set(defaultUser.id, defaultUser);
+// -----------------------------------------------------
+
+
+export class InMemoryUserRepository implements CreateUserRepository {
+  createUser(userData: CreateUserRepository.Request): Promise<UserEntity> {
+    const id = '321'
+    inMemoryDatabase.set(userData.email, { id, ...userData})
+
+    const user = new UserEntity({ id, email: userData.email })
+
+    return Promise.resolve(user)
+  }
+}
