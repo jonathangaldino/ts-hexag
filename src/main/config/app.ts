57cefbed26b85c6ca4d8f23a426b7d5ee1395014
Jonathan Galdino <johnzballad@gmail.com>
2022-11-27 17:21:45 -0300

    body parser middlewares for express app

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,7 +1,9 @@
 import express, { Express } from "express";
+import setupMiddlewares from "@main/config/middlewares";
 
 const setupApp = (): Express => {
   const app = express();
+  setupMiddlewares(app);
 
   return app;
 };
