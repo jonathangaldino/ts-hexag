d754823d64bf763c1a056c77babaea4c0c8fda91
Jonathan Galdino <johnzballad@gmail.com>
2022-11-27 01:05:04 -0300

    creating application/use-cases folder

---
+++ src/application/use-cases/user/CreateUser.ts
@@ -0,0 +1,13 @@
+import { CreateUserRepository } from "@src/application/interfaces/repositories/user/CreateUserRepository";
+import { CreateUserInterface } from "@src/application/interfaces/use-cases/user/CreateUserInterface";
+import { UserEntity } from "@src/domain/entities/UserEntity";
+
+export class CreateUser implements CreateUserInterface {
+  constructor(private readonly createUserRepository: CreateUserRepository) {}
+
+  execute(userData: CreateUserInterface.Request): Promise<UserEntity> {
+    return this.createUserRepository.createUser({
+      ...userData,
+    })
+  }
+}
